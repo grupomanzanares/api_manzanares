fdff0175ee35a080bec7f29877719304e282e136
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2025-03-19 11:44:18 -0500

    mejora update user

--- auth/controllers/userControler.js
+++ auth/controllers/userControler.js
@@ -77,11 +77,18 @@ const deleteUser = async(req, res) =>{
 
 const updateUser = async(req, res) =>{
     try {
-        const {id, ...body} = matchedData(req)
+        // const {id, ...body} = matchedData(req)
+
+        const { id, password, ...rest } = matchedData(req);
+        const body = { ...rest };
+
+        // Si se envía una nueva contraseña, se encripta
+        if (password) {
+            body.password = await encrypt(password);
+        }
+
         
-        const response = await User.update(body, {
-            where: { id }
-        })
+        const response = await User.update(body, { where: { id }})
         if (response[0] === 0) {
             return res.status(404).json({
                 message: 'Usuario no encontrado o sin cambios'
@@ -89,10 +96,15 @@ const updateUser = async(req, res) =>{
         }
 
         const updatedUser = await User.findByPk(id);
+        const data = {
+            token: await tokenSign(updatedUser),
+            user: updatedUser
+        }
 
         res.status(200).json({
             message: 'Usuario actualizado correctamente',
-            data: updatedUser
+            data
+
         }); 
     } catch (error) {
         handleHttpError(res, 'No se pudo actualiza al usuario, intenta otra vez')
