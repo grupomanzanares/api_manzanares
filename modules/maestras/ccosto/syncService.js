14055e049207aa8fb976516b922cdf9777d24525
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2025-04-08 08:06:30 -0500

    Sincronizacion de Ccostos desde yeminus web

---
+++ modules/maestras/ccosto/syncService.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import db from '../../../config/db.js';
+
+import dotenv from "dotenv";  //importar modulo dotenv para llamar las variables de entorno
+import { ccosto,  empresa }  from "../masterRelations.js";
+
+
+
+dotenv.config();
+
+const externalBaseUrl = process.env.YEMINUS_URL
+const externalUsername = process.env.YEMINUS_USER
+const externalPassword = process.env.YEMINUS_PASSWORD
+
+
+async function getToken() {
+    const data = {
+        userName: externalUsername,
+        password: externalPassword,
+        grant_type: "password"
+};
+
+const response = await axios.post(`${externalBaseUrl}/token`, data, {
+    headers: { 'Content-Type': 'application/json' }
+    });
+
+    return response.data.access_token;
+}
+
+async function fetchCCostos(token) {
+    const response = await axios.post(
+            `${externalBaseUrl}/app/informes/informespersonalizados/ejecutarconsultainforme`,
+        {
+            crearArchivo: false,
+            idInforme: 53
+        },
+        {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json",
+                "id_empresa": "03", // ajusta según lógica dinámica si es necesario
+                "usuario": externalUsername
+            }
+        }
+    );
+
+    return response.data;
+}
+
+async function syncCCostos() {
+    try {
+        const token = await getToken();
+        const ccostosData = await fetchCCostos(token);
+
+        for (const item of ccostosData) {
+            const nit = item.EMPRESA_NIT.trim();;
+            const centro = item.CENTRO.trim();;
+            const ncentro = item.NCENTRO.trim();;
+            const estadoRaw = item.ESTADO.trim();;
+            const estado = estadoRaw === "A" ? true : false;
+
+        const empresaDb  = await empresa.findOne({ where: { nit } });
+        if (!empresaDb) {
+            console.warn(`Empresa con NIT ${nit} no encontrada`);
+            continue;
+        }
+
+        await ccosto.upsert({
+            codigo: centro,
+            nombre: ncentro,
+            estado: estado === "1",
+            empresaId: empresaDb.id
+        });
+        }
+
+        console.log("Sincronización completada correctamente.");
+    } catch (err) {
+        console.error("Error en sincronización:", err);
+    }
+}
+
+module.exports = { syncCCostos };
+
+/** Instalar  npm install node-cron */
